c12af93a7bfc06361b31222b08926568f873d254
Srijan Shovit <srijanshovit@gmail.com>
2021-12-26 17:43:25 +0530

    style props

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {Heading} from './components/Heading';
 import { Oscar } from './components/Oscar';
 import { Button } from './components/Button';
 import { Input } from './components/Input';
+import { Container } from './components/Container';
 
 function App() {
   const personName = {
@@ -44,6 +45,8 @@ function App() {
       }}/>
 
       <Input value='sri' handleChange={(event)=> console.log(event)}/>
+
+      <Container styles={{border: '1px solid black',padding: '1rem'}} />
     </div>
   );
 }
